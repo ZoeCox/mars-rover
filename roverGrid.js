f30c267e8f1a4ec2775af6cbc2f37082e23fd581
Zoe Cox <zoemcox16@gmail.com>
2025-12-01 17:18:11 +0000

    update

--- roverGrid.js
+++ roverGrid.js
@@ -1,8 +1,14 @@
+const roverImg = new Image();
+roverImg.src = "./assets/roverImg.png";
+// roverImg.style.objectFit = "scale";
 const grid = document.querySelector("#grid");
 for (let i = 0; i < 100; i++) {
   const divElement = document.createElement("div");
   divElement.id = `grid-div-${i}`;
-  console.log(divElement.id);
-  divElement.innerHTML = i;
+  // console.log(divElement.id);
+  divElement.innerHTML = i + 1;
   grid.appendChild(divElement);
 }
+
+const block1 = document.querySelector("#grid-div-0");
+block1.appendChild(roverImg);
